0bd8cea6e25aa3e381227cc2b2685d42d46fc4fc
KarloFab <fabijanickarlo1@gmail.com>
2021-02-03 22:38:35 +0100

    Rearranged depdendencies and added playground dependendy

--- src/main/java/com/kfabija/graphqltutorial/model/Vehicle.java
+++ src/main/java/com/kfabija/graphqltutorial/model/Vehicle.java
@@ -1,32 +1,14 @@
 package com.kfabija.graphqltutorial.model;
 
+import lombok.Builder;
+import lombok.Value;
+
+@Value
+@Builder
 public class Vehicle {
 
-    private Long id;
-    private String name;
-    private VehicleType vehicleType;
+    Long id;
+    String name;
+    VehicleType vehicleType;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public VehicleType getVehicleType() {
-        return vehicleType;
-    }
-
-    public void setVehicleType(VehicleType vehicleType) {
-        this.vehicleType = vehicleType;
-    }
 }
--- src/main/java/com/kfabija/graphqltutorial/resolver/VehicleResolver.java
+++ src/main/java/com/kfabija/graphqltutorial/resolver/VehicleResolver.java
@@ -1,10 +1,18 @@
 package com.kfabija.graphqltutorial.resolver;
 
+import com.kfabija.graphqltutorial.model.Vehicle;
+import com.kfabija.graphqltutorial.model.VehicleType;
 import graphql.kickstart.tools.GraphQLQueryResolver;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 public class VehicleResolver implements GraphQLQueryResolver {
 
+    public Vehicle getVehicle(Long id) {
+        log.info("Retrieving vehicle by id: {}", id);
 
+        return Vehicle.builder().id(id).vehicleType(VehicleType.CAR).name("VW").build();
+    }
 }
