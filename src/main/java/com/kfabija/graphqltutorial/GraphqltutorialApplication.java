8fb64f02ba4757677b66dae8b7c6437dc01dd669
karlofabijanic <karlo.fabijanic@deegloo.com>
2021-02-03 22:03:48 +0100

    initial commit

---
+++ src/main/java/com/kfabija/graphqltutorial/GraphqltutorialApplication.java
@@ -0,0 +1,13 @@
+package com.kfabija.graphqltutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqltutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphqltutorialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kfabija/graphqltutorial/GraphqltutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kfabija.graphqltutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqltutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
