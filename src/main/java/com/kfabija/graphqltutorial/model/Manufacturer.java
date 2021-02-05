7e0820fb1d52de614f3398c9266734118ead119c
KarloFab <fabijanickarlo1@gmail.com>
2021-02-05 18:12:09 +0100

    Added jpa annotations on models

--- src/main/java/com/kfabija/graphqltutorial/model/Manufacturer.java
+++ src/main/java/com/kfabija/graphqltutorial/model/Manufacturer.java
@@ -3,13 +3,19 @@ package com.kfabija.graphqltutorial.model;
 import lombok.Builder;
 import lombok.Value;
 
+import javax.persistence.*;
 import java.util.Set;
 
+@Entity
 @Value
 @Builder
 public class Manufacturer {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     Long id;
     String name;
+
+    @OneToMany
     Set<Vehicle> vehicles;
 }
--- src/main/java/com/kfabija/graphqltutorial/model/Vehicle.java
+++ src/main/java/com/kfabija/graphqltutorial/model/Vehicle.java
@@ -3,12 +3,19 @@ package com.kfabija.graphqltutorial.model;
 import lombok.Builder;
 import lombok.Value;
 
+import javax.persistence.*;
+
+@Entity
 @Value
 @Builder
 public class Vehicle {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     Long id;
     String name;
     VehicleType type;
+
+    @ManyToOne
     Manufacturer manufacturer;
 }
