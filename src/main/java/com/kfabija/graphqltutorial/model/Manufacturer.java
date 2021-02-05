7c9986beb10b82c85ed9bd32d737f4e0c85c8e72
KarloFab <fabijanickarlo1@gmail.com>
2021-02-05 18:03:04 +0100

    Added manufacturer model

--- src/main/java/com/kfabija/graphqltutorial/model/Manufacturer.java
+++ src/main/java/com/kfabija/graphqltutorial/model/Manufacturer.java
@@ -3,12 +3,13 @@ package com.kfabija.graphqltutorial.model;
 import lombok.Builder;
 import lombok.Value;
 
+import java.util.Set;
+
 @Value
 @Builder
-public class Vehicle {
+public class Manufacturer {
 
     Long id;
     String name;
-    VehicleType type;
-    Manufacturer manufacturer;
+    Set<Vehicle> vehicles;
 }
