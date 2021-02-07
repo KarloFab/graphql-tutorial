5c722b7248ad2e51d83dbff4b336f84e2a92b65d
KarloFab <fabijanickarlo1@gmail.com>
2021-02-07 18:24:38 +0100

    Added additional methods to query

--- src/main/java/com/kfabija/graphqltutorial/resolver/Query.java
+++ src/main/java/com/kfabija/graphqltutorial/resolver/Query.java
@@ -1,8 +1,33 @@
 package com.kfabija.graphqltutorial.resolver;
 
+import com.kfabija.graphqltutorial.model.Manufacturer;
+import com.kfabija.graphqltutorial.model.Vehicle;
+import com.kfabija.graphqltutorial.repository.ManufacturerRepository;
 import com.kfabija.graphqltutorial.repository.VehicleRepository;
 import graphql.kickstart.tools.GraphQLQueryResolver;
 
 public class Query implements GraphQLQueryResolver {
     private VehicleRepository vehicleRepository;
+    private ManufacturerRepository manufacturerRepository;
+
+    public Query(VehicleRepository vehicleRepository, ManufacturerRepository manufacturerRepository) {
+        this.vehicleRepository = vehicleRepository;
+        this.manufacturerRepository = manufacturerRepository;
+    }
+
+    public Iterable<Vehicle> findAllVehicles(){
+        return vehicleRepository.findAll();
+    }
+
+    public Iterable<Manufacturer> findAllManufactures(){
+        return manufacturerRepository.findAll();
+    }
+
+    public long countVehicles() {
+        return vehicleRepository.count();
+    }
+
+    public long countManufacturers(){
+        return manufacturerRepository.count();
+    }
 }
